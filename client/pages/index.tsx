2fd5ed4f5a0598b5bbe4db71bd68df15f6d5f205
Nicky Lai <nicky.lai@lloydsbanking.com>
2021-09-20 10:06:01 +0100

    Commited client 2

---
+++ client/pages/index.tsx
@@ -0,0 +1,51 @@
+import { useRef, useEffect } from "react";
+import Head from "next/head";
+import Image from "next/image";
+import styles from "../styles/Home.module.css";
+import { useSockets } from "../context/socket.context";
+
+import RoomsContainer from "../containers/Rooms";
+import MessagesContainer from "../containers/Messages";
+
+export default function Home() {
+  const { socket, username, setUsername } = useSockets();
+
+  const usernameRef = useRef(null);
+
+  useEffect(() => {
+    if (usernameRef)
+      usernameRef.current.value = localStorage.getItem("username") || "";
+  }, []);
+
+  function handleSetUsername() {
+    const value = usernameRef.current.value;
+    if (!value) {
+      return;
+    }
+
+    setUsername(value);
+
+    localStorage.setItem("username", value);
+  }
+
+  return (
+    <div>
+      {!username && (
+        <div className={styles.usernameWrapper}>
+          <div className={styles.usernameInner}>
+            <input placeholder="Username" ref={usernameRef} />
+            <button className="cta" onClick={handleSetUsername}>
+              START
+            </button>
+          </div>
+        </div>
+      )}
+      {username && (
+        <div className={styles.container}>
+          <RoomsContainer />
+          <MessagesContainer />
+        </div>
+      )}
+    </div>
+  );
+}
